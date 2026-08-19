02277916ceb2e28679f18cbd5847af19b22e1c22
El Equinox <el_equinox_36@outlook.com>
2026-08-19 20:40:35 +0100

    Add files via upload
    Add my first Python project

---
+++ equinox_profile.py
@@ -0,0 +1,15 @@
+Python 3.13.15 (tags/v3.13.15:4061bc4, Aug  5 2026, 13:05:39) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/HomePC/AppData/Local/Programs/Python/Python313/equinox_profile.py
+What is your name? Equinox
+How old are you? 20
+What is your biggest goal? Study Computer Science abroad
+What are you passionate about? Music production and Computer Science
+
+===== EQUINOX PROFILE =====
+Name: Equinox
+Age: 20
+Goal: Study Computer Science abroad
+Passion: Music production and Computer Science
+===========================
